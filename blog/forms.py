1df76e0565093bbac0d1a39a05e15890976abe48
rinkurajole <raj.rajole@gmail.com>
2015-11-19 16:47:01 +0530

    gui improved

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,12 @@
 from django import forms
-
 from .models import Post, Comment
+from django.contrib.auth.models import User
+
+class UserForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ('username', 'password',)
 
 class PostForm(forms.ModelForm):
 
