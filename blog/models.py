c6898c982a8b97611cf0e76772c619eb2b1ba2a8
rinkurajole <raj.rajole@gmail.com>
2015-11-26 11:37:11 +0530

    post,comment gui changed

--- blog/models.py
+++ blog/models.py
@@ -24,7 +24,7 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', related_name='comments')
-    author = models.CharField(max_length=200)
+    author = models.ForeignKey('auth.User')
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     approved_comment = models.BooleanField(default=False)
--- blog/views.py
+++ blog/views.py
@@ -85,14 +85,13 @@ def add_comment_to_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
         form = CommentForm(request.POST)
+        print(request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
+            comment.author = request.user
             comment.post = post
             comment.save()
             return redirect('blog.views.post_detail', pk=post.pk)
-    else:
-        form = CommentForm()
-    return render(request, 'blog/add_comment_to_post.html', {'form': form})
 
 @login_required
 def comment_approve(request, pk):
